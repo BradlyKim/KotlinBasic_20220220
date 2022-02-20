57defe43e1478435e72145ac732213915b8eca81
BradlyKim <kjh5864@naver.com>
2022-02-20 14:19:08 +0900

    var / val 변수 만들고 대입해보기 - val는 한번만 대입 가능

--- app/src/main/java/com/neppplus/kotlinbasic_20220220/MainActivity.kt
+++ app/src/main/java/com/neppplus/kotlinbasic_20220220/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -28,8 +29,25 @@ class MainActivity : AppCompatActivity() {
 //            토스트를 띄워보자
 
             Toast.makeText(this, "토스트 버튼 눌림", Toast.LENGTH_SHORT).show()
-            
 
+        }
+
+        btnVariablePractice.setOnClickListener {
+
+//            var / val 에 대한 연습 체험
+            var myName : String   // 내용 변경 가능
+            val friendName : String  // 한번 대입한 값으로 고정
+            
+//            myName - 내 이름, friendName - 친구 이름
+            
+            myName = "김정환"
+            friendName = "전혜진"
+
+//            내이름 / 친구이름 다시 대입 (데이터 변경)
+            
+            myName = "김도훈"  // var은 데이터 변경 가능
+//          friendName = "김시연"  // val은 데이터 변경 불가
+            
         }
 
     }
